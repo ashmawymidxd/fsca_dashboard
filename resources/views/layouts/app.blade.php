a1ddca0a7c095511dcd258426fe94f5fd658f502
ahmed hassan <uyu365222@gmail.com>
2025-08-01 14:00:48 +0300

    move css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     <link href="{{ asset('argon') }}/vendor/nucleo/css/nucleo.css" rel="stylesheet">
     <link href="{{ asset('argon') }}/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
     <!-- Argon CSS -->
-    <link type="text/css" href="{{ asset('argon/css/argon.css?v=1.0.0') }}" rel="stylesheet">
+    <link type="text/css" href="{{ asset('assets/argon.css?v=1.0.0') }}" rel="stylesheet">
     {{-- Data tables --}}
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
     <link rel="stylesheet" href="https://cdn.datatables.net/2.0.2/css/dataTables.bootstrap4.css">
